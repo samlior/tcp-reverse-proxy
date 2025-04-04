e8f905bd918069f1842c8f776f627679fc3d0fad
samlior <samlior@foxmail.com>
2025-04-04 19:51:21 +0800

    Add test code

--- cmd/entry-point/main.go
+++ cmd/entry-point/main.go
@@ -1,7 +1,73 @@
 package main
 
-import "fmt"
+import (
+	"crypto/ed25519"
+	"crypto/tls"
+	"crypto/x509"
+	"encoding/binary"
+	"flag"
+	"log"
+	"os"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	serverCert := flag.String("server-cert", "cert/server.crt", "server certificate path")
+	authPrivateKey := flag.String("auth-private-key", "cert/auth", "auth private key path")
+	serverAddress := flag.String("server-address", "localhost:4433", "server address")
+
+	flag.Parse()
+
+	serverCertBytes, err := os.ReadFile(*serverCert)
+	if err != nil {
+		log.Fatal("failed to read server certificate:", err)
+	}
+	authPrivateKeyBytes, err := os.ReadFile(*authPrivateKey)
+	if err != nil {
+		log.Fatal("failed to read auth private key:", err)
+	}
+
+	certPool := x509.NewCertPool()
+	ok := certPool.AppendCertsFromPEM(serverCertBytes)
+	if !ok {
+		log.Fatal("failed to append the server certificate")
+	}
+
+	conn, err := tls.Dial("tcp", *serverAddress, &tls.Config{
+		RootCAs: certPool,
+	})
+	if err != nil {
+		log.Fatal("failed to connect to relay server:", err)
+	}
+
+	defer conn.Close()
+
+	buffer := make([]byte, 1024)
+	n, err := conn.Read(buffer)
+	if err != nil {
+		log.Fatal("failed to read from relay server:", err)
+	}
+
+	signature := ed25519.Sign(authPrivateKeyBytes, buffer[:n])
+
+	write := func(data []byte) (int, error) {
+		prefix := make([]byte, 2)
+		binary.BigEndian.PutUint16(prefix, uint16(len(data)))
+		return conn.Write(append(prefix, data...))
+	}
+
+	n, err = write(signature)
+	if err != nil {
+		log.Fatal("failed to write to relay server:", err)
+	}
+
+	log.Println("wrote size:", n)
+
+	tick := time.NewTicker(time.Second)
+	defer tick.Stop()
+
+	for {
+		<-tick.C
+		write([]byte("hello"))
+	}
 }
--- cmd/relay-server/main.go
+++ cmd/relay-server/main.go
@@ -1,8 +1,174 @@
 package main
 
-import "fmt"
+import (
+	"crypto/ed25519"
+	"crypto/rand"
+	"crypto/tls"
+	"encoding/binary"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	serverCert := flag.String("server-cert", "cert/server.crt", "server certificate path")
+	serverKey := flag.String("server-key", "cert/server.key", "server key path")
+	authPublicKey := flag.String("auth-public-key", "cert/auth.pub", "auth public key path")
+	host := flag.String("host", "0.0.0.0", "host")
+	port := flag.Int("port", 4433, "port")
+
+	flag.Parse()
+
+	serverCertBytes, err := os.ReadFile(*serverCert)
+	if err != nil {
+		log.Fatal("failed to read server certificate:", err)
+	}
+	serverKeyBytes, err := os.ReadFile(*serverKey)
+	if err != nil {
+		log.Fatal("failed to read server key:", err)
+	}
+	authPublicKeyBytes, err := os.ReadFile(*authPublicKey)
+	if err != nil {
+		log.Fatal("failed to read auth public key:", err)
+	}
+
+	cert, err := tls.X509KeyPair(serverCertBytes, serverKeyBytes)
+	if err != nil {
+		log.Fatalf("failed to create x509 key pair:", err)
+	}
+
+	listener, err := tls.Listen("tcp", fmt.Sprintf("%s:%d", *host, *port), &tls.Config{
+		Certificates: []tls.Certificate{cert},
+	})
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	defer listener.Close()
+
+	log.Printf("listening on %s:%d...", *host, *port)
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Printf("failed to accept connection:", err)
+			continue
+		}
+		go handleConnection(authPublicKeyBytes, conn)
+	}
 }
 
+type Message struct {
+	Flag uint8
+	Data []byte
+}
+
+func parseMessage(bytes []byte) (*Message, error) {
+	if len(bytes) < 2 {
+		return nil, fmt.Errorf("invalid message")
+	}
+
+	return &Message{Flag: bytes[0], Data: bytes[1:]}, nil
+}
+
+func handleConnection(authPublicKeyBytes []byte, conn net.Conn) {
+	defer conn.Close()
+
+	log.Println("client connected:", conn.RemoteAddr())
+
+	randomBytes := make([]byte, 32)
+
+	_, err := rand.Read(randomBytes)
+	if err != nil {
+		log.Println("failed to generate random bytes:", err)
+		return
+	}
+
+	// send challenge
+	conn.Write(randomBytes)
+
+	ch := make(chan []byte)
+
+	go func() {
+		defer close(ch)
+
+		buffer := make([]byte, 1024)
+		cursor := 0
+		length := 0
+		left := 0
+		pending := []byte{}
+		var err error
+
+		read := func(n int) []byte {
+			slice := buffer[cursor : cursor+n]
+			cursor += n
+			length -= n
+			return slice
+		}
+
+		for {
+			length, err = conn.Read(buffer)
+			if err == io.EOF {
+				// closed
+				return
+			}
+			if err != nil {
+				log.Println("error reading from client:", err)
+				return
+			}
+
+			for length > 0 {
+				if left == 0 {
+					if length < 2 {
+						log.Println("client sent invalid message")
+						return
+					}
+
+					left = int(binary.BigEndian.Uint16(read(2)))
+				}
+
+				var readLength int
+				if left > length {
+					readLength = length
+				} else {
+					readLength = left
+				}
+
+				pending = append(pending, read(readLength)...)
+
+				left -= readLength
+
+				if left == 0 {
+					ch <- pending
+
+					pending = []byte{}
+				}
+			}
+
+			cursor = 0
+			length = 0
+		}
+	}()
+
+	// wait for challenge answer
+	select {
+	case <-time.After(time.Second):
+		log.Println("client challenge timed out")
+		return
+	case answer := <-ch:
+		// verify challenge answer
+		if !ed25519.Verify(authPublicKeyBytes, randomBytes, answer) {
+			log.Println("client challenge verification failed")
+			return
+		}
+	}
+
+	for bytes := range ch {
+		// TODO
+		log.Println("received message:", string(bytes))
+	}
+}
